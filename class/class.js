90814a86abfb6a7b12aac2b4c33922f6925e86f2
Nick Tsai <myintaer@gmail.com>
2017-10-14 16:50:42 +0800

    Update class.js

--- class/class.js
+++ class/class.js
@@ -6,91 +6,35 @@
  * items you designed by different options.
  *
  * @author      Nick Tsai <myintaer@gmail.com>
- * @filesource  jQuery
  */
 var Class = function (options) {
 
-    "user strict";
+  "use strict";
+  var self = this;
+  // Application Options initialize
+  var options = options || {};
 
-    // Create self constant in class
-    var self = this;
+  /* 
+   * Private Propertites 
+   */
+  // var privateAttr = 1;
 
-    /* Private Propertites */
+  /* 
+   * Public Propertites 
+   */
+  // publicAttr = options.publicAttr || null;
 
-    // initialize options parameter for definition
-    var o = options || {};
+  /* Constructor */
+  var constructor = function(options) {
 
-    // Flag for loadData()
-    var isLoading = false;
+    self.initialize();
+  }
+  /* Controller */
+  self.initialize = function() {
 
-
-    /* Public Propertites */
-
-    // Tree data
-    self.data = o.data || null;
-
-    // AJAX URL for loadData()
-    self.ajaxUrl = o.ajaxUrl || '/org3layermenu/ajax';
-
-
-    /* Constructor */
-    var __constructor = function () {
-
-        // Construct code
-    }
-
-    /* <public method> Initialize with callback function */
-    self.init = function (callback) {
-
-        loadData(callback);
-    }
-
-    /* <public method> Render */
-    self.render = function (options) {
-
-        // Load data if no data 
-        if (self.data===null) {
-
-            loadData(function() {
-                self.render(options)
-            });
-
-            return;
-        }
-
-        console.log(self.data);
-
-        // Render code
-    }
-
-    /* <private method> Load data from AJAX */
-    var loadData = function (callback) {
-
-        // Check if is loading
-        if (isLoading) {
-
-            // Retry later
-            setTimeout(function(){
-
-                callback();
-            }, 500);
-
-            return;
-        }
-
-        isLoading = true;
-
-        // Load data by AJAX
-        $.get( self.ajaxUrl, function(data) {
-
-            self.data = data;
-
-            isLoading = false;
-
-            callback();
-        });
-    }
-
-    // Activate constructor
-    __constructor();
+    // Init actions
+  }
+  
+  // Implement Constructor
+  constructor(options);
 }
